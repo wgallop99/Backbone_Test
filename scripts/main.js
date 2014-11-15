2866d1c6747b2225afb781f521bac452fefb26a9
Will Gallop <wgallop99@gmail.com>
2014-11-15 10:31:42 -0500

    resolving errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,36 @@
+
+var Item = Backbone.Model.extend({});
+
 var collection = Backbone.Collection.extend({
-  model:model,
-  url: '..api/posts.json'
+  model: Item,
+  url: 'api/posts.json'
 });
 
 var list = new collection();
 
+var itemView = Backbone.View.extend({
+  template: _.template('<p><%= title %></p>'),
+
+
+  render: function(){
+    this.$el.html(this.template(this.model));
+    return this;
+  }
+});
+
+
+var collectionView = Backbone.View.extend({
+  render: function(){
+    this.collection.forEach(this.addOne, this);
+    return this;
+  },
+  addOne: function(model){
+    var itemView = new ItemView({model: model});
+    itemView.render()
+    this.$el.append(itemView.el);
+  }
+});
+
 list.fetch();
+
+$('#app').html(collectionView.render().el);
