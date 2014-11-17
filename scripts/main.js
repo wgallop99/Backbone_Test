564124f52c8b537bc0418a5ae47c629ea3873cfa
Will Gallop <wgallop99@gmail.com>
2014-11-17 09:33:33 -0500

    troubleshooting

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var Item = Backbone.Model.extend({
 
 })
 
-var collection = Backbone.Collection.extend({
+var newsCollection = Backbone.Collection.extend({
   initialize: function () {
         console.log('initiliazed collection')
   },
@@ -16,8 +16,6 @@ var collection = Backbone.Collection.extend({
   model: Item,
   url: 'api/posts.json'
 });
-
-var list = new collection();
 
 var itemView = Backbone.View.extend({
   template: _.template('<p><%= title %></p>'),
@@ -42,6 +40,12 @@ var collectionView = Backbone.View.extend({
   }
 });
 
-list.fetch();
+var finalListView = new collectionView({collection: newsCollection});
+
+// var list = new collectionView();
+
+$('#app').html(finalListView.render().el);
+
+// finalListView.fetch();
 
 // $('#app').html(collectionView.render().el);
